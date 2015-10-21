81dc796e4c652e7c6eea01e99422538a92aa6c1a
DeepaRavi <ota830@my.utsa.edu>
2015-10-21 13:21:12 -0500

    CreateMessageActivity and createmessage layout

---
+++ app/src/main/java/com/asap/messenger/CreateMessageActivity.java
@@ -0,0 +1,13 @@
+package com.asap.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.asap.messenger.helper.MessageHelper;
+
+/**
+ * Created by shaanu on 10/21/2015.
+ */
+public class CreateMessageActivity extends AppCompatActivity {
+    MessageHelper messageHelper = new MessageHelper();
+
+}
