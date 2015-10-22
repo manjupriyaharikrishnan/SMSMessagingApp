438d328014280d997f364543b34248f6c6068de5
DeepaRavi <ota830@my.utsa.edu>
2015-10-22 13:43:02 -0500

    modifications to create message xml and activity

--- app/src/main/java/com/asap/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/asap/messenger/CreateMessageActivity.java
@@ -1,11 +1,14 @@
 package com.asap.messenger;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 
 import com.asap.messenger.helper.MessageHelper;
 
@@ -21,6 +24,19 @@ public class CreateMessageActivity extends AppCompatActivity {
         setContentView(R.layout.createmessage);
 
         Message currentmessage=new Message();
+
+        final EditText editText = (EditText)findViewById(R.id.EditText);
+        editText.setHint("Type Message");
+        editText.setTextColor(Color.GRAY);
+
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                editText.setText("");
+                return false;
+            }
+        });
+
 
     }
 
