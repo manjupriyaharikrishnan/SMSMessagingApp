643cca99634a58b63ad1077b8b7f320aa62433c1
DeepaRavi <ota830@my.utsa.edu>
2015-11-15 14:11:32 -0600

    MessageHelper Test cases

--- app/src/test/java/com/asap/messenger/helper/test/MessageHelperTest.java
+++ app/src/test/java/com/asap/messenger/helper/test/MessageHelperTest.java
@@ -1,7 +1,9 @@
 package com.asap.messenger.helper.test;
 
+import android.database.Cursor;
 import android.renderscript.Long2;
 import android.support.annotation.NonNull;
+import android.view.Menu;
 
 import com.asap.messenger.bo.Message;
 import com.asap.messenger.common.MessageStatus;
@@ -9,19 +11,24 @@ import com.asap.messenger.helper.MessageHelper;
 
 import junit.framework.Assert;
 
+import static com.asap.messenger.helper.MessageHelper.getDateForDisplay;
 import static com.asap.messenger.helper.MessageHelper.isToday;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+
+import dalvik.annotation.TestTarget;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -33,134 +40,152 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by Umadevi on 10/20/2015.
  */
+
 public class MessageHelperTest {
 
     MessageHelper messageHelper = new MessageHelper();
 
-
     @Test
-    public void testdeletemessagebyId() {
-        List<Message> originalMessagelist = new ArrayList<Message>();
-        for (Message msg : originalMessagelist) {
-            if (msg.getMessageId() == 2)
-                originalMessagelist.remove(msg);
-            assertNotNull(originalMessagelist);
-        }
+    public void testisnotNumericwithstring() {
+        String str = new String();
+        str = "111-222-aaaa";
+        assertFalse(MessageHelper.isNumeric((str)));
     }
 
     @Test
-    public void testnotdeletemessagebyId() {
-        int a = 0;
-        List<Message> originalMessagelist = new ArrayList<Message>();
-        for (Message msg : originalMessagelist) {
-            if (msg.getMessageId() == a)
-                originalMessagelist.remove(msg);
-            assertNull(originalMessagelist);
-        }
+    public void testisnotNumericwithspecialcharacters() {
+        String str = new String();
+        str = "111-$$$-aaaa";
+        assertFalse(MessageHelper.isNumeric((str)));
     }
 
-
-    @Test
-    public void testcheckifmsgislocked() {
-        List<Message> originalMessagelist = new ArrayList<Message>();
-        for (Message msg : originalMessagelist) {
-            if (msg.getMessageId() == 2 && msg.isLocked()) {
-                assertTrue(true);
-                assertNotEquals(false, true);
-
-            }
-        }
-    }
 
     @Test
     public void testisNumeric() {
-        assertFalse(MessageHelper.isNumeric(("111-111-1111")));
+        String str = new String();
+        str = "1112223333";
+        assertTrue(MessageHelper.isNumeric((str)));
     }
 
     @Test
-    public void testisnotNumeric() {
-        assertFalse(MessageHelper.isNumeric(("a111111111")));
+    public void testisnotNumericwithhyphen() {
+        String str = new String();
+        str = "111-222-3333";
+        assertFalse(MessageHelper.isNumeric((str)));
     }
 
     @Test
-    public void testisToday() {
-        Date givendate = new Date(07 / 11 / 2015);
-        Calendar givenDateCal = Calendar.getInstance();
-        givenDateCal.setTime(givendate);
+    public void testgetdatefordisplaycase2() {
+        MessageHelper messageHelper = new MessageHelper();
+        long inputDate = 01 - 02;
+        Date longDate = new Date(inputDate);
+        String formatteddate = null;
+        Boolean a = isToday(longDate);
+
+        DateFormat targetFormat = new SimpleDateFormat("11 09");
+        formatteddate = targetFormat.format(longDate);
+
+        assertNotNull(messageHelper.getDateForDisplay(inputDate), a);
+
+    }
+
+    @Test
+    public void testgetdatefordisplayequalvalue() {
+        MessageHelper messageHelper = new MessageHelper();
+        long inputDate = 01 - 02;
+        Date longDate = new Date(inputDate);
+        String formatteddate = null;
+        Boolean a = isToday(longDate);
+        DateFormat targetFormat = new SimpleDateFormat("11 09");
+        formatteddate = targetFormat.format(longDate);
+
+        assertNotEquals(messageHelper.getDateForDisplay(inputDate), a);
+
+    }
+
+    @Test
+    public void testisTodaynotsame() throws ParseException {
+
+        String inputStr = "11-11-2012";
+        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        Date givenDate = dateFormat.parse(inputStr);
+
+        Calendar givendatecal = Calendar.getInstance();
+        givendatecal.setTime(givenDate);
 
         Calendar todayDateCal = Calendar.getInstance();
         todayDateCal.setTime(new Date());
 
+        if (givendatecal.get(Calendar.ERA) == todayDateCal.get(Calendar.ERA)
+                && givendatecal.get(Calendar.YEAR) == todayDateCal.get(Calendar.YEAR)
+                && givendatecal.get(Calendar.DAY_OF_YEAR) == todayDateCal.get(Calendar.DAY_OF_YEAR))
+            ;
+        {
 
-        if (givenDateCal.get(Calendar.ERA) == todayDateCal.get(Calendar.ERA) &&
-                givenDateCal.get(Calendar.YEAR) == todayDateCal.get(Calendar.YEAR)
-                && givenDateCal.get(Calendar.DAY_OF_YEAR) == todayDateCal.get(Calendar.DAY_OF_YEAR)) {
-            assertTrue(true);
+            assertFalse(messageHelper.isToday(givenDate));
         }
     }
 
     @Test
-    public void testisnotToday() {
-        Date givendate = new Date(07 / 11 / 2015);
-        Calendar givenDateCal = Calendar.getInstance();
-        givenDateCal.setTime(givendate);
+    public void testisTodaysame() throws ParseException {
+
+        String inputStr = "11-15-2015";
+        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        Date givenDate = dateFormat.parse(inputStr);
+
+        Calendar givendatecal = Calendar.getInstance();
+        givendatecal.setTime(givenDate);
 
         Calendar todayDateCal = Calendar.getInstance();
         todayDateCal.setTime(new Date());
 
+        if (givendatecal.get(Calendar.ERA) == todayDateCal.get(Calendar.ERA)
+                && givendatecal.get(Calendar.YEAR) == todayDateCal.get(Calendar.YEAR)
+                && givendatecal.get(Calendar.DAY_OF_YEAR) == todayDateCal.get(Calendar.DAY_OF_YEAR))
+            ;
+        {
 
-        if (givenDateCal.get(Calendar.ERA) == todayDateCal.get(Calendar.ERA) &&
-                givenDateCal.get(Calendar.YEAR) == todayDateCal.get(Calendar.YEAR)
-                && givenDateCal.get(Calendar.DAY_OF_YEAR) == todayDateCal.get(Calendar.DAY_OF_YEAR)) {
-            assertFalse(true);
+            assertTrue(messageHelper.isToday(givenDate));
         }
     }
 
-
     @Test
-    public void testisnotcorrectToday() {
-        Date givendate = new Date(07 / 2011 / 15);
-        Calendar givenDateCal = Calendar.getInstance();
-        givenDateCal.setTime(givendate);
+    public void testisTodaynotsamewithstring() throws ParseException {
+
+        String inputStr = "11-15-20aa";
+        DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        Date givenDate = dateFormat.parse(inputStr);
+
+        Calendar givendatecal = Calendar.getInstance();
+        givendatecal.setTime(givenDate);
 
         Calendar todayDateCal = Calendar.getInstance();
         todayDateCal.setTime(new Date());
 
+        if (givendatecal.get(Calendar.ERA) == todayDateCal.get(Calendar.ERA)
+                && givendatecal.get(Calendar.YEAR) == todayDateCal.get(Calendar.YEAR)
+                && givendatecal.get(Calendar.DAY_OF_YEAR) == todayDateCal.get(Calendar.DAY_OF_YEAR))
+            ;
+        {
 
-        if (givenDateCal.get(Calendar.ERA) == todayDateCal.get(Calendar.ERA) &&
-                givenDateCal.get(Calendar.YEAR) == todayDateCal.get(Calendar.YEAR)
-                && givenDateCal.get(Calendar.DAY_OF_YEAR) == todayDateCal.get(Calendar.DAY_OF_YEAR)) {
-            assertFalse(true);
-        }
-    }
-
-    @Test
-    public void testgetdatefordisplay() {
-        Date longDate = new Date(07 / 11 / 2015);
-        String formattedDate = null;
-
-        if (isToday(longDate)) {
-            DateFormat targetFormat = new SimpleDateFormat("2:15 a");
-            formattedDate = targetFormat.format(longDate);
-            assertNotNull(formattedDate);
-        } else {
-            DateFormat targetFormat = new SimpleDateFormat("02 03");
-            formattedDate = targetFormat.format(longDate);
-            assertNotNull(formattedDate);
+            assertFalse(messageHelper.isToday(givenDate));
         }
     }
 
 
     @Test
-    public void testgetmessagebycontact() {
-        List<Message> originalMessagelist = new ArrayList<Message>();
-        List<Message> Messagelist = new ArrayList<Message>();
-
-        String contact = "aaa";
-        for (Message msg : originalMessagelist) {
-            if (msg.getMessageAddress().equals(contact)) {
-                Boolean a = Messagelist.add(msg);
-                assertFalse(a);
+    public void testdeleteMessageById()
+    {
+        int id=1;
+        List<Message> originalMessageList = new ArrayList<>();
+        originalMessageList.add(new Message(0, "hello", "5554", new Date().getTime(), MessageStatus.LOCK));
+        originalMessageList.add(new Message(1, "hello", "5554", new Date().getTime(), MessageStatus.SENT));
+        List<Message> templist=new ArrayList<>();
+        templist.addAll(originalMessageList);
+        for(Message message : originalMessageList){
+            if(message.getMessageId()==1){
+                originalMessageList.remove(message);
+                assertEquals(messageHelper.deleteMessageById(id,templist), originalMessageList );
             }
         }
 
